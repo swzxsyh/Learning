756348d44203f29d38ed3e8f37088103849ceaec
swzxsyh <swzxsyh@gmail.com>
2018-10-25 12:15:55 +0800

    ADD C#

---
+++ C_Sharp/First.cs
@@ -0,0 +1,15 @@
+using System;
+
+
+inamespace Wrox 
+{
+	public  class MyFirstClass
+	{
+		static void Main()
+		{
+			Console.WriteLine("Hello! from Wrox.");
+			Console.ReadLine();
+			return;
+		}
+	}
+}
